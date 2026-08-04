de0707ec3956e17508c91352d9ed4c098f6e7c63
Matheuszin <matheus.com.0512@gmail.com>
2026-08-04 14:34:49 -0300

    alteracao

--- T.I/COBTI001 - Gerador etiquetas/FRONTEND/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-const PageMain = dynamic(() => import('../../components/pageMain'), { ssr: false })
-export default function screenMain() {
-  return (
-    <PageMain />
-  )
-}
